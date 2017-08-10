d6eb0a66ecbac3f07ee2702fe4d7750abb9b590f
avance <austin.vance@fishbowlinventory.com>
2017-08-10 09:56:20 -0600

    created and filled out passport logic file, created empty endpoints for login and register

--- controllers/UserControl.js
+++ controllers/UserControl.js
@@ -1,5 +1,11 @@
 var User = require('../models/User.js');
 
 module.exports = {
+    login: function() {
+
+    },
+    register: function() {
+
+    }
 
 }
--- server.js
+++ server.js
@@ -7,18 +7,30 @@ var express = require('express')
 ,	secret = require('./secret.js')
 ,	mongooseURI = secret.mongooseURI
 ,	passport = require('passport')
-,	passportLocal = require('passport-local')
 ,	session = require('express-session')
 ,	needle = require('needle')
+,	flash = require('connect-flash')
 ,   PostControl = require('./controllers/PostControl.js')
 ,   AdminControl = require('./controllers/AdminControl.js')
+,   UserControl = require('./controllers/UserControl.js')
 ,	app = express();
+
+
+require('./config/passport')(passport);
+app.use(session({ secret: 'secretsecret'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use(bodyParser.json());
 app.use(cors())
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
+
+//PASSPORT AUTH
+app.get('/login', UserControl.login);
+app.post('/register', UserControl.register);
 
 //POSTS
 app.post('/api/create-post', PostControl.newPost);
