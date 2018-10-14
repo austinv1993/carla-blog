2b439e6e8a7f23b2c25b8d6503236e9617b2c7f4
austin <d.austin.vance@gmail.com>
2018-10-14 16:20:01 -0600

    pagination in list view

--- public/javascript/angular-c/listCtrl.js
+++ public/javascript/angular-c/listCtrl.js
@@ -1,8 +1,19 @@
 angular.module('blog')
 .controller('listCtrl', function($scope, $state, postSrvc) {
 
-    $scope.posts = [];
     $scope.search = "";
+    $scope.posts = [];
+    $scope.displayedPosts = [];
+    getPosts();
+
+    $scope.displayAmount = 5;
+    var postCount = 0;
+    var startIndex = 0;
+    var endIndex = 0;
+    $scope.displayedPosts = [];
+
+    $scope.hasMore = false;
+    $scope.hasLess = false;
 
     $scope.createPost = function() {
         $state.go("create");
@@ -16,12 +27,37 @@ angular.module('blog')
         $state.go("main");
     };
 
-    getPosts();
+    $scope.paginate = function (goForward) {
+        if (goForward) {
+            startIndex = endIndex;
+            endIndex = (endIndex + $scope.displayAmount) < postCount ? (endIndex + $scope.displayAmount) : postCount;
+            $scope.displayedPosts = $scope.posts.slice(startIndex, endIndex);
+            $scope.hasMore = endIndex < postCount;
+            $scope.hasLess = true;
+        } else {
+            startIndex = (startIndex - $scope.displayAmount) < 0 ? 0 : startIndex - $scope.displayAmount;
+            endIndex = (startIndex + $scope.displayAmount) < postCount ? (startIndex + $scope.displayAmount) : postCount;
+            $scope.displayedPosts = $scope.posts.slice(startIndex, endIndex);
+            $scope.hasMore = true;
+            $scope.hasLess = startIndex > 0;
+        }
+        enableButtons();
+    };
 
     function getPosts() {
         postSrvc.getAll().then(function (posts) {
             $scope.posts = posts;
+            postCount = $scope.posts.length;
+            endIndex = $scope.displayAmount < postCount ? $scope.displayAmount : postCount;
+            $scope.displayedPosts = $scope.posts.slice(startIndex, endIndex);
+            $scope.hasMore = $scope.displayAmount < postCount;
+            enableButtons();
         })
+    }
+
+    function enableButtons() {
+        document.getElementById("Previous").disabled = !$scope.hasLess;
+        document.getElementById("Next").disabled = !$scope.hasMore;
     }
 
 });
